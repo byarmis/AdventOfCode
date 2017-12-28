98e98d6e2414a0c171b37d963b1a662c96156878
Ben Yarmis <byarmis@indeed.com>
2017-12-27 18:07:11 -0600

    Day 20, part 1

---
+++ AoC2017/Day_20/Day_20.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = int(x)
+        self.y = int(y)
+        self.z = int(z)
+
+    @property
+    def dist(self):
+        return abs(self.x) + abs(self.y) + abs(self.z)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x,
+                      self.y + other.y,
+                      self.z + other.z)
+    def __str__(self):
+        return 'Vector({},{},{})'.format(self.x, self.y, self.z)
+
+class Particle:
+    def __init__(self, num, position, velocity, acceleration):
+        self.num = num
+        self.pos = position
+        self.vel = velocity
+        self.acc = acceleration
+
+    def __repr__(self):
+        return '{} Particle({},{},{})'.format(self.num, self.pos, self.vel, self.acc)
+
+    def tick(self):
+        self.vel += self.acc
+        self.pos += self.vel
+
+    def __lt__(self, other):
+        return self.pos.dist < other.pos.dist
+    def __le__(self, other):
+        return self.pos.dist <= other.pos.dist
+    def __eq__(self, other):
+        return self.pos.dist == other.pos.dist
+    def __ne__(self, other):
+        return self.pos.dist != other.pos.dist
+    def __gt__(self, other):
+        return self.pos.dist > other.pos.dist
+    def __ge__(self, other):
+        return self.pos.dist >= other.pos.dist
+
+REGEX = r'.*<(.*)>.*<(.*)>.*<(.*)>.*'
+
+def part_1(filename):
+    with open(filename) as f:
+        f = f.readlines()
+
+    
+    particles = []
+    for i, line in enumerate(f):
+        _, p, v, a, _ = re.split(REGEX, line)
+
+        p = Vector(*p.split(','))
+        v = Vector(*v.split(','))
+        a = Vector(*a.split(','))
+
+        particles.append(Particle(i, p, v, a))
+
+
+    closest = min(particles)
+
+    while True:
+        for _ in range(1000):
+            _ = [p.tick() for p in particles]
+
+            new_closest = min(particles)
+
+            if new_closest is not closest:
+                closest = new_closest
+                # Closest changed, keep ticking
+                break
+        else:
+            # If we go 1000 cycles without the closest particle chaning, we're good
+            break
+
+    return closest
+
+if __name__ == '__main__':
+    tst = part_1('test.txt')
+    assert tst.num == 0, tst.num
+
+    print('Part One:{}'.format(part_1('input.txt')))
+
