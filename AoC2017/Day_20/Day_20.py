dd4af0630bd06b4498082aa7147841c7fde7d10c
Ben Yarmis <ben@yarm.is>
2017-12-27 18:55:01 -0600

    Day 20, pt 2

--- AoC2017/Day_20/Day_20.py
+++ AoC2017/Day_20/Day_20.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+from collections import Counter
 
 class Vector:
     def __init__(self, x, y, z):
@@ -18,7 +19,7 @@ class Vector:
                       self.y + other.y,
                       self.z + other.z)
     def __str__(self):
-        return 'Vector({},{},{})'.format(self.x, self.y, self.z)
+        return '({},{},{})'.format(self.x, self.y, self.z)
 
 class Particle:
     def __init__(self, num, position, velocity, acceleration):
@@ -38,14 +39,22 @@ class Particle:
         return self.pos.dist < other.pos.dist
     def __le__(self, other):
         return self.pos.dist <= other.pos.dist
-    def __eq__(self, other):
-        return self.pos.dist == other.pos.dist
     def __ne__(self, other):
         return self.pos.dist != other.pos.dist
     def __gt__(self, other):
         return self.pos.dist > other.pos.dist
     def __ge__(self, other):
         return self.pos.dist >= other.pos.dist
+
+    def __eq__(self, other):
+        return self.pos.x == other.pos.x\
+                and self.pos.y == other.pos.y\
+                and self.pos.z == other.pos.z
+
+
+    def __hash__(self):
+        return hash((self.pos.x, self.pos.y, self.pos.z))
+                
 
 REGEX = r'.*<(.*)>.*<(.*)>.*<(.*)>.*'
 
@@ -83,9 +92,50 @@ def part_1(filename):
 
     return closest
 
+def part_2(filename):
+    with open(filename) as f:
+        f = f.readlines()
+    
+    particles = []
+    for i, line in enumerate(f):
+        _, p, v, a, _ = re.split(REGEX, line)
+
+        p = Vector(*p.split(','))
+        v = Vector(*v.split(','))
+        a = Vector(*a.split(','))
+
+        particles.append(Particle(i, p, v, a))
+
+    num_particles = len(particles)
+
+    while True:
+        for _ in range(10):
+            _ = [p.tick() for p in particles]
+
+            cnt = Counter(particles)
+            dupes = [p for p, c in cnt.items() if c > 1]
+
+            particles = [p for p in particles if p not in dupes]
+
+            new_num = len(particles)
+
+            if new_num != num_particles:
+                num_particles = new_num
+                break
+        else:
+            break
+    return num_particles
+
+
+
 if __name__ == '__main__':
     tst = part_1('test.txt')
     assert tst.num == 0, tst.num
 
     print('Part One:{}'.format(part_1('input.txt')))
+
+    tst = part_2('test2.txt')
+    assert tst == 1, tst
+
+    print('Part Two:{}'.format(part_2('input.txt')))
 
