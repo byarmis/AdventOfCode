e6d51301033461e3c53567ff932b74d3bcaa046c
Ben Yarmis <byarmis@indeed.com>
2018-06-11 21:56:24 -0500

    Day 21 complete

--- AoC2017/Day_21/Day_21.py
+++ AoC2017/Day_21/Day_21.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from itertools import product
-from pprint import pprint
 
 class Array:
     def __init__(self, a):
@@ -14,30 +13,29 @@ class Array:
             self._a = a
 
         self.cnt = sum(sum(i) for i in self._a)
-        self.len = len(self._a)
         self.hash = hash(tuple(tuple(i) for i in self._a))
 
     def __hash__(self):
         return self.hash
 
     def __len__(self):
-        return self.len
-
-    def __iter__(self):
-        return self._a
+        return len(self._a)
 
     @staticmethod
     def process_string(string):
         return [[j=='#' for j in i.strip()] for i in string.split('/')]
 
-    # def __repr__(self):
-        # return '\n'.join(str(a) for a in self._a)
+    def __repr__(self):
+        return '\n'.join(str(a) for a in self._a)
 
     def __eq__(self, other):
-        if self.len != other.len:
+        if len(self) != len(other):
             return False
 
         if self.cnt != other.cnt:
+            return False
+
+        if hash(self) != hash(other):
             return False
 
         for i in range(len(self._a)):
@@ -60,12 +58,16 @@ class Array:
         # Column index becomes the row index
         # abs(Row index - len) becomes the col index
         o = [[None for _ in self._a[0]] for _ in self._a]
+        assert len(o) == len(o[0])
 
         for row_idx, row in enumerate(self._a):
             for col_idx, val in enumerate(row):
-                o[col_idx][abs(row_idx - self.len + 1)] = val
+                o[col_idx][abs(row_idx - len(self) + 1)] = val
 
         return Array(o)
+
+    def __getitem__(self, x):
+        return self._a[x]
 
 
 class Grid:
@@ -75,8 +77,11 @@ class Grid:
         self.perms = None
         self.generate_permutations()
 
+    def __repr__(self):
+        return self.array.__repr__()
+
     def __len__(self):
-        return self.array.len
+        return len(self.array)
 
     def __hash__(self):
         return hash(self.array)
@@ -95,19 +100,29 @@ class Grid:
 
             a = a.rotate
         
-        self.perms = perms
+        self.perms = tuple(perms)
 
     def __eq__(self, other):
+        if self.cnt != other.cnt:
+            return False
+
         for s, o in product(self.perms, other.perms):
             if s == o:
                 return True
 
         return False
 
+    def __getitem__(self, x):
+        return self.array[x]
+
+    @property
+    def cnt(self):
+        return self.array.cnt
+
 class Art:
     def __init__(self, d):
         self.grid = Grid('.#./..#/###')
-        self.size = 3
+        self.size = len(self.grid)
         self.enhancement_dict = d
 
     @property
@@ -126,41 +141,68 @@ class Art:
         else:
             raise NotImplementedError
 
-        row_start = 0
-        sub_arrays = []
+        def split(grid):
+            row_start = 0
+            sub_arrays = []
 
-        # Split into subgrids
-        while row_start < len(self.grid):
-            col_start = 0
-            row = []
-            while col_start < len(self.grid[0]):
-                row.append(Grid(self.grid[row_start:row_start+n][col_start:col_start+n]))
+            # Split into subgrids
+            while row_start < len(grid):
+                col_start = 0
+                row = []
+                while col_start < len(grid):
+                    new_subgrid = [x[col_start:col_start+n] for x in grid[row_start:row_start+n]]
 
-                col_start += n
+                    row.append(Grid(new_subgrid))
+                    col_start += n
 
-            sub_arrays.append(row)
+                sub_arrays.append(row)
 
-            row_start += n
+                row_start += n
 
-        # Grow the subgrids
-        new_arrays = []
-        for row in sub_arrays:
-            new_row = []
-            for array in row:
-                for k, v in self.enhancement_dict.items():
-                    if array == k:
-                        new_row.append(v)
-                        break
-                else:
-                    # There's no mapping, stick it on there anyways
-                    new_row.append(array)
-            new_arrays.append(new_row)
+            return sub_arrays
 
-        # Merge the subgrids
+        def _grow(sub_arrays):
+            # Grow the subgrids
+            new_arrays = []
+            for row in sub_arrays:
+                new_row = []
 
-        # Join everything back together
-        self.grid = [a.array._a for a in new_arrays]
-        print([len(i) for i in new_arrays])
+                for array in row:
+                    for k, v in self.enhancement_dict.items():
+                        if array == k:
+                            new_row.append(v)
+                            break
+                    else:
+                        raise AssertionError('No enhancement found')
+
+                new_arrays.append(new_row)
+
+            return new_arrays
+
+        def merge(new_arrays):
+            # Merge the subgrids
+            # Join everything back together
+
+            new_grid = []
+            big_row = 0
+
+            for big_row in new_arrays:
+                # Get the Nth row of each subgrid
+                n = 0
+                while n < len(big_row[0]):
+                    new_row = []
+                    for subgrid in big_row:
+                        new_row.extend(subgrid[n])
+                    new_grid.append(new_row)
+                    n += 1
+            return Grid(new_grid)
+
+
+        sub_arrays = split(self.grid)
+        new_arrays = _grow(sub_arrays)
+        new_grid   = merge(new_arrays)
+
+        self.grid = new_grid
 
 def get_enhancement_dict(filename):
     with open(filename) as f:
@@ -182,10 +224,23 @@ if __name__ == '__main__':
     ed = get_enhancement_dict('test.txt')
 
     a = Art(ed)
-    print('a')
     a.grow()
-    print(a.grid)
-    print('b')
     a.grow()
-    print('c')
+
+    assert a.on_count == 12, 'On-count must be 12'
+
+    ed = get_enhancement_dict('input.txt')
+    a = Art(ed)
+    for _ in range(5):
+        a.grow()
+
+    print('5 iterations', a.on_count)
+
+    a = Art(ed)
+
+    for i in range(18):
+        print(i)
+        a.grow()
+
+    print('18 iterations', a.on_count)
 
