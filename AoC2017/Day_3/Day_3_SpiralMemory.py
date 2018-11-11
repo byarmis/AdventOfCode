4d8d0ad417eda081fd8e149b98b092cd43e99cde
Ben Yarmis <ben@yarm.is>
2018-11-11 05:14:44 +0000

    Day 3, part 2 finally something presentable

--- AoC2017/Day_3/Day_3_SpiralMemory.py
+++ AoC2017/Day_3/Day_3_SpiralMemory.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import itertools
+from itertools import product
 
 def part_1(i):
     def add_another_layer(d):
@@ -99,179 +100,76 @@ def part_1(i):
 
     return steps(i)
 
-class Matrix(object):
-    def __init__(self, m):
-        self.m = m
-        self._max = max(itertools.chain(*m))
 
-    def max(self):
-        return self._max
+class Point:
+    def __init__(self, x, y=None):
+        self.x = x
+        self.y = y
 
-    def _allocate_next_ring(self):
-        '''
-        Takes a matrix, returns the same data, but with a ring of None-s added 
-        around the existing data.
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
 
-        Size of the matrix returned has two additional rows and each row is two 
-        longer than original.
-        '''
+    def __repr__(self):
+        return 'Point({x}, {y})'.format(x=self.x, y=self.y)
 
-        new_m = [[None for _ in range(len(self.m)+2)]]
-        for row in self.m:
-            new_m.append([None] + row + [None])
-        new_m.append([None for _ in range(len(self.m)+2)])
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
-        self.m = new_m
-
-    def add_ring(self):
-        self._allocate_next_ring()
-        self._add_right()
-        self._add_top()
-        self._add_left()
-        self._add_bottom()
-
-
-    def _add_right(self):
-        # Second from bottom right
-        self.m[-2][-1] = self.m[-2][-2] + self.m[-3][-2]
-
-        # Go up the right side
-        row = -3
-        while row > -len(self.m) + 1:
-            new_val = self.m[row][-2]    # Next to it 
-            new_val += self.m[row-1][-2] # Diagonal up
-            new_val += self.m[row+1][-2] # Diagonal down
-
-            new_val += self.m[row+1][-1] # Below it
-
-            self.m[row][-1] = new_val
-
-            row -= 1
-
-        # Second from top right
-        self.m[1][-1] = self.m[1][-2] + self.m[2][-2] + self.m[2][-1]
-
-        # Top right
-        self.m[0][-1] = self.m[1][-2] + self.m[1][-1]
-
-    def _add_top(self):
-        # Go along the top
-        # Second from top right
-        self.m[0][-2] = self.m[0][-1] + self.m[1][-2] + self.m[1][-1] + self.m[1][-3]
-
-        # Go along the top
-        col = -3
-        while col > -len(self.m[0]) + 1:
-            new_val = self.m[0][col+1]  # Right
-            new_val += self.m[1][col]   # Below
-            new_val += self.m[1][col+1] # Diagonal left
-            new_val += self.m[1][col-1] # Diagonal right
-
-            self.m[0][col] = new_val
-
-            col -= 1
-
-        # Second from top left
-        self.m[0][1] = self.m[0][2] + self.m[1][1] + self.m[1][2]
-
-        # Top left
-        self.m[0][0] = self.m[0][1] + self.m[1][1]
-
-    def _add_left(self):
-       # Go down the left
-        # Second from top left
-        self.m[1][0] = self.m[0][0] + self.m[0][1] + self.m[1][1] + self.m[2][1]
-
-        # Go down the left
-        row = 2
-        while row < len(self.m) - 2:
-            new_val = self.m[row][1]    # Right
-            new_val += self.m[row-1][1] # Diagonal up
-            new_val += self.m[row+1][1] # Diagonal down
-            new_val += self.m[row-1][0] # Above
-            
-            self.m[row][0] = new_val
-
-            row += 1
-
-        # Second from bottom left
-        self.m[-2][0] = self.m[-3][0] + self.m[-3][1] + self.m[-2][1]
-
-        # Bottom left corner
-        self.m[-1][0] = self.m[-2][0] + self.m[-2][1]
-
-    def _add_bottom(self):
-        # Go accross the bottom
-        # Second from bottom left
-        self.m[-1][1] = self.m[-2][0] + self.m[-1][0] + self.m[-2][1] + self.m[-2][2]
-
-        # Go along the bottom
-        col = 2
-        while col < len(self.m[0]) - 1:
-            new_val = self.m[-1][col-1]  # Left
-            new_val += self.m[-2][col-1] # Diagonal left
-            new_val += self.m[-2][col]   # Up
-            new_val += self.m[-2][col+1] # Diagonal right
-
-            self.m[-1][col] = new_val
-            col += 1
-
-        # Bottom right
-        self.m[-1][-1] = self.m[-1][-2] + self.m[-2][-2] + self.m[-2][-1]
-
-        self._max = self.m[-1][-1]
-
-    def search(self, i):
-        # Search right
-        prev = self.m[-2][-1]
-        for row in self.m[:-1][::-1]:
-            x = row[-1]
-            if prev < i < x:
-                return x
-            prev = x
-
-        # Search top
-        for x in self.m[0][::-1]:
-            if prev < i < x:
-                return x
-            prev = x
-
-        # Search left
-        for row in self.m:
-            x = row[0]
-            if prev < i < x:
-                return x
-            prev = x
-
-        # Search bottom
-        for x in self.m[-1]:
-            if prev < i < x:
-                return x
-            prev = x
-
-        raise ValueError('Unable to find {}'.format(i))
+    def __hash__(self):
+        return hash((self.x, self.y))
 
 def part_2(i):
+    DIRECTIONS = tuple(Point(p[0], p[1]) for p in product([-1,0,1], repeat=2) if p != (0,0))
 
-    matrix = Matrix([[  5,  4,  2],
-                     [ 10,  1,  1],
-                     [ 11, 23, 25]])
-    matrix.add_ring()
+    points = {Point(0,0): 1}
+    point_stack = [1]
+    r = 1
 
-    assert matrix.m == [[147, 142, 133, 122,  59],
-                        [304,   5,   4,   2,  57],
-                        [330,  10,   1,   1,  54],
-                        [351,  11,  23,  25,  26],
-                        [362, 747, 806, 880, 931]]
+    def neighbor_sum(p):
+        to_add = 0
+        for d in DIRECTIONS:
+            try:
+                to_add += points[p+d]
+            except KeyError:
+                continue
 
-    while matrix.max() < i:
-        matrix.add_ring()
+        return to_add
+     
+    while point_stack[-1] < i:
+        # Add right
+        for y in range(-r+1, r+1):
+            ns = neighbor_sum(Point(r, y))
+            points[Point(r, y)] = ns
+            point_stack.append(ns)
 
-    return matrix.search(i)
+        # Add top, left to right, except the top corner
+        for x in range(r, -r-1, -1):
+            ns = neighbor_sum(Point(x, r))
+            points[Point(x, r)] = ns
+            point_stack.append(ns)
+
+        # Add left, top to bottom
+        for y in range(r, -r-1, -1):
+            ns = neighbor_sum(Point(-r, y))
+            points[Point(-r, y)] = ns
+            point_stack.append(ns)
+
+        # Add bottom
+        for x in range(-r, r+1):
+            ns = neighbor_sum(Point(x, -r))
+            points[Point(x, -r)] = ns
+            point_stack.append(ns)
+
+        r += 1
+
+    while point_stack[-1] > i:
+        # Pop from the stack until we go less than the value we're looking for
+        ret_val = point_stack.pop()
+
+    return ret_val
 
 if __name__ == '__main__':
     p = 361527
     print(part_1(p))
-
     print(part_2(p))
 
