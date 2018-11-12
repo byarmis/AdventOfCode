46288ce8e990b2112c3cf1c24e78fb63d9b35283
Ben Yarmis <ben@yarm.is>
2018-11-12 06:43:56 +0000

    Day 6, now py3 and a little cleaner

--- AoC2017/Day_6/Day_6_MemoryReallocation.py
+++ AoC2017/Day_6/Day_6_MemoryReallocation.py
@@ -1,49 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def reallocate_mem(b):
-    # Get idx of first max
-    m = max(b)
-    for loc, val in enumerate(b):
-        if val == m:
-            break
+from collections import namedtuple
 
-    amt = val
-    b[loc] = 0
+def reallocate_mem(bank):
+    # Get idx of first max
+    amt = max(bank)
+    idx = bank.index(amt)
+
+    bank[idx] = 0
 
     # While we have memory, run through, allocating
     while amt > 0:
-        loc += 1
-        if loc == len(b):
-            loc = 0
+        idx += 1
+        idx %= len(bank) # Wrap around
 
-        b[loc] += 1
+        bank[idx] += 1
         amt -= 1
 
+def reallocate_cycle_count(bank):
+    seen_configs = set()
+    count = 0
 
-def reallocate_cycle_count(b):
-    seen_configs = {tuple(b),}
+    while tuple(bank) not in seen_configs:
+        seen_configs.add(tuple(bank))
+        reallocate_mem(bank)
+        count += 1
 
-    reallocate_mem(b)
-
-    c = 1
-    while tuple(b) not in seen_configs:
-        seen_configs.add(tuple(b))
-        reallocate_mem(b)
-        c += 1
-
-    return c, b
+    return namedtuple('result', 'count final_val')(count, bank)
 
 
 if __name__ == '__main__':
     test_bank = [0,2,7,0]
 
-    assert reallocate_cycle_count(test_bank)[0] == 5
-    assert reallocate_cycle_count(reallocate_cycle_count(test_bank)[1])[0] == 4
+    assert reallocate_cycle_count(test_bank).count == 5
+    assert reallocate_cycle_count(reallocate_cycle_count(test_bank).final_val).count == 4
 
     p = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14]
 
-    p1 = reallocate_cycle_count(p)
-    print 'Part one:{}'.format(p1[0])
-    print 'Part two:{}'.format(reallocate_cycle_count(p1[1])[0])
-
+    res = reallocate_cycle_count(p)
+    print('Part one:{}'.format(res.count))
+    print('Part two:{}'.format(reallocate_cycle_count(res.final_val).count))
