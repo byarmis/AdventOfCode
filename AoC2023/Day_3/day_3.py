dfda703534727da9c401a74af3f8cfabd5328647
Ben Yarmis <ben@yarm.is>
2023-12-03 02:14:02 -0600

    not a fan

--- AoC2023/Day_3/day_3.py
+++ AoC2023/Day_3/day_3.py
@@ -1,54 +1,44 @@
+import itertools
 
 with open('input.txt') as f:
-    lines = f.readlines()
+    lines = [line.strip() for line in f.readlines()]
 
-def part_1(lines):
+def solve(lines):
     nums = []
     for y, line in enumerate(lines):
         num = ''
-        for x, char in enumerate(line.strip()):
+        for x, char in enumerate(line):
             if char.isdigit():
                 num += char
             if (char.isdigit() and x == len(line) - 1) or (not char.isdigit() and num):
-                dx = [0]
-                dy = [0]
-
-                if x > 0:
-                    dx.append(-1)
-                if y > 0:
-                    dy.append(-1)
-                if x < len(line) - 1:
-                    dx.append(1)
-                if y < len(lines) - 1:
-                    dy.append(1)
+                dx = [-1, 0, 1]
+                dy = [-1, 0, 1]
 
                 for num_idx in range(1, len(num) + 2 - int(char.isdigit())):
                     dx.append(-num_idx)
 
                 candidates = []
-                for x_mod in dx:
-                    for y_mod in dy:
-                        if char.isdigit():
-                            x_mod_limit = 0 >= x_mod > -len(num)
-                        else:
-                            x_mod_limit = 0 > x_mod >= -len(num) 
+                for x_mod, y_mod in itertools.product(dx, dy):
+                    if char.isdigit():
+                        x_mod_limit = 0 >= x_mod > -len(num)
+                    else:
+                        x_mod_limit = 0 > x_mod >= -len(num) 
 
-                        if x_mod_limit and y_mod == 0:
-                            continue
-                        if x + x_mod < 0 or x+x_mod > len(line) - 1:
-                            continue
-                        if y + y_mod < 0 or y+y_mod > len(lines) - 1:
-                            continue
+                    if x_mod_limit and y_mod == 0:
+                        continue
+                    if x + x_mod < 0 or x+x_mod > len(line) - 1:
+                        continue
+                    if y + y_mod < 0 or y+y_mod > len(lines) - 1:
+                        continue
 
-                        candidates.append(lines[y+y_mod][x+x_mod])
-                if any(x != '.' and not x.isdigit() for x in candidates): 
+                    candidates.append(lines[y+y_mod][x+x_mod])
+                if any(x != '.' and not x.isdigit() for x in candidates):
                     nums.append(int(num))
 
                 num = ''
     return sum(nums)
 
 test_lines_1 = [
-        '.........', 
         '467..114.', 
         '...*.....', 
         '..35..633', 
@@ -60,24 +50,37 @@ test_lines_1 = [
         '...$.*...', 
         '.664.598.', 
         '.+12....1', 
-        '.1.....+1', 
+        '11.....+1', 
         '1......1+', 
         ]
 test_lines_2 = [line+'.' for line in test_lines_1]
 test_lines_3 = ['.'+line for line in test_lines_1]
 test_lines_4 = ['.'+line+'.' for line in test_lines_1]
 
-test_lines = [test_lines_1, test_lines_2, test_lines_3, test_lines_4]
+test_lines_5 = [
+        '..4361....',
+        '.24+++2...',
+        ]
+
+
+test_lines = [
+        test_lines_1,
+        test_lines_2,
+        test_lines_3,
+        test_lines_4,
+        test_lines_5,
+        ]
 
 for loc, test_line in enumerate(test_lines):
-    test = part_1(test_line)
-    assert  test == 4361+16, f'{loc}: {4361+14} expected, got {test}'
+    test = solve(test_line)
+    assert  test == 4361+26, f'{loc}: {4361+14} expected, got {test}'
 
 print('tests pass')
 
-assert part_1(lines) != 554613
-assert part_1(lines) != 552006
-assert part_1(lines) != 552463
-assert part_1(lines) != 536628
-print('Part 1: ', part_1(lines))
+assert solve(lines) != 554613
+assert solve(lines) != 552006
+assert solve(lines) != 552463
+assert solve(lines) != 536628
+assert solve(lines) != 536171
+print('Part 1: ', solve(lines))
 
