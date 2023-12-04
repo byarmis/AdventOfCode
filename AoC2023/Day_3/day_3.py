94325f00714f066279c399b5f3b307d29ad92943
Ben Yarmis <byarmis@indeed.com>
2023-12-04 10:25:51 -0600

    Holy shit, I found the bug thank god

--- AoC2023/Day_3/day_3.py
+++ AoC2023/Day_3/day_3.py
@@ -11,35 +11,32 @@ def solve(lines):
             if char.isdigit():
                 num += char
             if (char.isdigit() and x == len(line) - 1) or (not char.isdigit() and num):
-                dx = {-1, 0, 1}
+                dx = {-1, 0}
                 dy = {-1, 0, 1}
+
+                if char.isdigit():
+                    dx.add(1)
 
                 for num_idx in range(int(char.isdigit()), len(num) + 2 - int(char.isdigit())):
                     dx.add(-num_idx)
 
-                candidates = []
+                candidates = set()
                 for x_mod, y_mod in itertools.product(dx, dy):
-                    if char.isdigit():
-                        x_mod_limit = 0 >= x_mod > -len(num)
-                    else:
-                        x_mod_limit = 0 > x_mod >= -len(num) 
-
-                    if x_mod_limit and y_mod == 0:
-                        continue
                     if x + x_mod < 0 or x+x_mod > len(line) - 1:
                         continue
                     if y + y_mod < 0 or y+y_mod > len(lines) - 1:
                         continue
 
-                    candidates.append(lines[y+y_mod][x+x_mod])
+                    candidates.add(lines[y+y_mod][x+x_mod])
 
-                if any(x != '.' and not x.isdigit() for x in candidates):
+                if any(candidates - set('1234567890.')):
                     nums.append(int(num))
 
                 num = ''
+
     return sum(nums)
 
-test_lines_1 = [
+provided_lines = [
         '467..114.', 
         '...*.....', 
         '..35..633', 
@@ -50,46 +47,36 @@ test_lines_1 = [
         '......755', 
         '...$.*...', 
         '.664.598.', 
-        '.+12....1', 
-        '11.....+1', 
-        '1......11', 
         ]
 
-test_lines_2 = [line+'.' for line in test_lines_1]
-test_lines_3 = ['.'+line for line in test_lines_1]
-test_lines_4 = ['.'+line+'.' for line in test_lines_1]
+test_cases = {
+        4361 : provided_lines,
+        4361 : [line+'.' for line in provided_lines],
+        4361 : ['.'+line for line in provided_lines],
+        4361 : ['.'+line+'.' for line in provided_lines],
+        123+45+6 : [
+            '.123...',
+            '45++6..',
+            ],
+        123: [
+            '..+..',
+            '.123.',
+            ],
+        0 : [
+            '....+.',
+            '123...',
+            ],
+        }
 
-test_lines_5 = [
-        '..4361....',
-        '.35+++1...',
-        ]
+for expected, test_lines in test_cases.items():
+    test = solve(test_lines)
+    assert  test == expected, f'{expected} expected, got {test}'
 
-test_lines_6 = [
-        '..+..',
-        '4397.',
-        ]
-
-
-test_lines = [
-        test_lines_1,
-        test_lines_2,
-        test_lines_3,
-        test_lines_4,
-        test_lines_5,
-        test_lines_6,
-        ]
-
-for loc, test_line in enumerate(test_lines):
-    test = solve(test_line)
-    expected = 4361+12+22+2
-    assert  test == expected, f'{loc}: {expected} expected, got {test}'
-
-print('tests pass')
-
-assert solve(lines) != 554613
-assert solve(lines) != 552006
-assert solve(lines) != 552463
-assert solve(lines) != 536628
-assert solve(lines) != 536171
+solution = solve(lines)
+assert solution != 554613, solution 
+assert solution != 552006, solution 
+assert solution != 552463, solution 
+assert solution != 536628, solution 
+assert solution != 536171, solution 
 print('Part 1: ', solve(lines))
 
