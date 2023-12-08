0afbe317d694d337803add20d6076aeba39dd176
Ben Yarmis <byarmis@indeed.com>
2023-12-08 14:46:06 -0600

    Might as well put this all in one big file, at least for now

--- HanukkahOfData/solutions_5784.py
+++ HanukkahOfData/solutions_5784.py
@@ -5,6 +5,31 @@ def getter(filename, pk):
         reader = csv.DictReader(f)
         return {line[pk]:line for line in reader}
 
+def night_1():
+    customers = getter('5784/noahs-customers.csv', 'customerid')
+    letters_to_num = {
+            'a':2, 'b':2, 'c':2,
+            'd':3, 'e':3, 'f':3,
+            'g':4, 'h':4, 'i':4,
+            'j':5, 'k':5, 'l':5,
+            'm':6, 'n':6, 'o':6,
+            'p':7, 'q':7, 'r':7, 's':7,
+            't':8, 'u':8, 'v':8,
+            'w':9, 'x':9, 'y':9, 'z':9,
+            }
+
+    def name_as_number(name):
+        return ''.join(str(letters_to_num.get(l.lower(), '')) for l in name.split()[1] if l)
+
+    filtered_customers = [customer['name'] for customer in customers.values() if name_as_number(customer['name']) == ''.join(l for l in customer['phone'] if l.isdigit())]
+
+    assert len(filtered_customers) == 1
+
+    return filtered_customers.pop()
+
+print('Night 1: ', night_1())
+
+
 def night_2():
     customers = getter('5784/noahs-customers.csv', 'customerid')
     orders = getter('5784/noahs-orders.csv', 'orderid')
