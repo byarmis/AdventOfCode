23dd178e4e3aa14ab21516140fdc800cdc44ee22
Ben Yarmis <ben@yarm.is>
2023-12-12 01:03:34 -0600

    I don't want to do dynamic programming though

--- AoC2023/Day_12/day_12.py
+++ AoC2023/Day_12/day_12.py
@@ -10,13 +10,60 @@ def springs_to_groupings(springs):
     for val in springs:
         if val == '#':
             g += 1
-        elif g > 0:
+        elif val == '.' and g > 0:
             gs.append(g)
             g = 0
+        elif val == '?':
+            break
+
     if g > 0:
         gs.append(g)
-
     return gs
+
+
+def springs_to_potential_groupings(springs):
+    if '?' not in springs:
+        yield springs_to_groupings(springs)
+
+    potential_groupings = set()
+    for loc, val in enumerate(springs):
+        if val == '?':
+            new_spring = springs.copy()
+            new_spring[loc] = '#'
+            yield from springs_to_potential_groupings(new_spring)
+            new_spring[loc] = '.'
+            yield from springs_to_potential_groupings(new_spring)
+
+
+class PartialList:
+    def __init__(self, vals):
+        self.vals = vals
+
+    def __eq__(self, other):
+        if len(self.vals) == len(other.vals):
+            return self.vals == other.vals
+        if len(self.vals) < len(other.vals):
+            return self.vals == other.vals[:len(self.vals)]
+        else:
+            return other.vals == self.vals[:len(other.vals)]
+
+def springs_to_partial_groupings(springs):
+    g = 0
+    gs = []
+
+    for val in springs:
+        if val == '#':
+            g += 1
+        elif val == '.' and g > 0:
+            gs.append(g)
+            g = 0
+        elif val == '?':
+            break
+
+    if g > 0:
+        gs.append(g)
+    return PartialList(gs)
+
 
 def get_arrangements(line):
     springs, groupings = line.split()
@@ -33,6 +80,8 @@ def get_arrangements(line):
     generated = set()
 
     while to_generate:
+        if len(to_generate) % 1000000 == 0:
+            print(len(to_generate)//1000000)
         new_spring = to_generate.popleft()
         if '?' not in new_spring:
             generated.add(tuple(new_spring))
@@ -42,17 +91,19 @@ def get_arrangements(line):
             if val == '?':
                 a = new_spring.copy()
                 b = new_spring.copy()
+
                 a[loc] = '#'
-                to_generate.append(a)
+                if groupings in springs_to_potential_groupings(a):
+                    to_generate.append(a)
 
                 b[loc] = '.'
-                to_generate.append(b)
+                if groupings in springs_to_potential_groupings(b):
+                    to_generate.append(b)
 
     print(f'generated - {len(generated)}')
     filtered = len([s for s in generated if springs_to_groupings(s) == groupings])
     print(f'filtered - {filtered}')
     return filtered
-
 
 
 def part_1(lines):
@@ -77,4 +128,8 @@ assert springs_to_groupings('.###.##....#') == [3,2,1]
 
 t = part_1(test_lines)
 assert t == 21, t
+
+print('Tests pass')
+
+print('Part 1: ', part_1(lines))
 
