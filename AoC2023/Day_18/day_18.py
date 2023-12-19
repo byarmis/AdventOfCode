5d3b4629eb8fcf39770ad37f45adb048ff316707
Ben Yarmis <byarmis@indeed.com>
2023-12-18 22:28:00 -0600

    should be passing, by all accounts >:(

--- AoC2023/Day_18/day_18.py
+++ AoC2023/Day_18/day_18.py
@@ -2,12 +2,26 @@
 with open('input.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 
+def shoelace(grid):
+    grid = list(grid)
+    area = 0
+
+    for i, j in zip(grid, grid[1:]):
+        area += (i[1]+j[1]) * (i[0] - j[0])
+    i = grid[-1]
+    j = grid[0]
+    area += (i[1]+j[1]) * (i[0] - j[0])
+    print('a',area)
+
+    return abs(area)
 
 def part_1(lines):
     grid = set()
 
     pnt = [0,0]
     grid.add(tuple(pnt))
+
+    perimeter = 0
 
     for line in lines:
         d, num, _ = line.split()
@@ -16,9 +30,9 @@ def part_1(lines):
         dx = 0
         dy = 0
         if d == 'U':
-            dy = 1
-        elif d == 'D':
             dy = -1
+        elif d == 'D':
+            dy = 1
         elif d == 'L':
             dx = -1
         elif d == 'R':
@@ -26,25 +40,13 @@ def part_1(lines):
         else:
             raise Exception('Unknown direction')
 
-        for _ in range(num):
-            pnt[0] += dx
-            pnt[1] += dy
-            grid.add(tuple(pnt))
+        pnt[0] += dx * num
+        pnt[1] += dy * num
+        grid.add(tuple(pnt))
+        perimeter += num
 
-    vol = 0
-
-    y_min = min(p[1] for p in grid)
-    y_max = max(p[1] for p in grid)
-
-    for row in range(y_min, y_max+1):
-        x_min = min(p[0] for p in grid if p[1] == row)
-        x_max = max(p[0] for p in grid if p[1] == row)
-
-        to_add = x_max - x_min + 1
-        vol += to_add
-
-    return vol
-
+    print('p',perimeter)
+    return shoelace(grid) + perimeter
 
 test_lines = [
     'R 6 (#70c710)',
@@ -66,6 +68,25 @@ test_lines = [
 
 t = part_1(test_lines)
 assert t == 62, t
+
+chrstine_test = [
+    'R 3 x', 
+    'D 3 x', 
+    'R 3 x', 
+    'U 3 x', 
+    'R 3 x', 
+    'D 9 x', 
+    'L 3 x', 
+    'U 3 x', 
+    'L 3 x', 
+    'D 3 x', 
+    'L 3 x', 
+    'U 9 x', 
+    ]
+
+t = part_1(chrstine_test)
+
+assert t == 88, t
 
 p1 = part_1(lines)
 assert p1 != 87847
